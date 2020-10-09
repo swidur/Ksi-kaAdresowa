4a64eb925652cb1d2d0cafa34051eb3b974ac091
świdur <dawid@swidurski.pl>
2020-10-10 00:07:54 +0200

    Added simple main view mockup

---
+++ KsiążkaAdresowaGUI/Classes/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KsiążkaAdresowaGUI.Classes
+{
+    class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string FullName { get { return $"{FirstName} {LastName}"; } }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+    }
+}
--- KsiążkaAdresowaGUI/MainWindow.xaml.cs
+++ KsiążkaAdresowaGUI/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using KsiążkaAdresowaGUI.Classes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,40 @@ namespace KsiążkaAdresowaGUI
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<Person> people = new List<Person>();
         public MainWindow()
         {
             InitializeComponent();
+
+            people.Add(new Person { FirstName = "Andrzej", LastName = "Lepper", Email = "alepper@polskawies.gov", Phone = "+48 699 488 251" });
+            people.Add(new Person { FirstName = "Ireneusz", LastName = "Lis", Email = "lis@gmail.com", Phone = "+48 699 231 251" });
+            people.Add(new Person { FirstName = "Tadeusz", LastName = "Kościuszko", Email = "", Phone = "+48 111 000 111" });
+            people.Add(new Person { FirstName = "Roman", LastName = "Giertych", Email = "maturatobzdura@buziaczek.pl", Phone = "+48 444 488 251" });
+
+            ListOfContacts.ItemsSource = people;
+        }
+
+        private void AddContact_Clik(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Contacts_Clik(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Export_Clik(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Import_Clik(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Bin_Clik(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
