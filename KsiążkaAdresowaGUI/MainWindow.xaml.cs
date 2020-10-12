aec4b530039ae75a6e7524fe0bf9f80b70517cf9
świdur <dawid@swidurski.pl>
2020-10-12 13:55:24 +0200

    Added core functionalities: dynamic search, active and deleted contacts, context menu on datagrid, crud operations on contacts

--- KsiążkaAdresowaGUI/MainWindow.xaml.cs
+++ KsiążkaAdresowaGUI/MainWindow.xaml.cs
@@ -1,15 +1,11 @@
-﻿using AutoMapper;
-using KADataAccess;
+﻿using KADataAccess;
 using KADataAccess.Models;
 using KARepository.Infrastructure.Repositories.Implementations;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Windows;
-using KARepository.Infrastructure.Profiles;
-using KARepository.DTOs;
 using System.Collections.ObjectModel;
 using System.Windows.Controls;
+using KsiążkaAdresowaGUI.Windows;
 
 namespace KsiążkaAdresowaGUI
 {
@@ -19,61 +15,53 @@ namespace KsiążkaAdresowaGUI
     public partial class MainWindow : Window
     {
         public ObservableCollection<Contact> people;
-
-        ContactEFRepo repo = new ContactEFRepo(new KAContext());
-        Mapper mapper;
+        ContactEFRepo repo;
         public bool showActiveContacts = true;
+        public Contact selectedEdit;
 
         public MainWindow()
         {
-            var config = new MapperConfiguration(cfg => cfg.AddProfile(new ContactProfile()));
-            mapper = new Mapper(config);
-
+            repo = new ContactEFRepo(new KAContext());
         }
 
         private void SearchBox_TextChanged(object sender, TextChangedEventArgs e)
         {
             if (showActiveContacts)
             {
-                UpdateActiveContacts(SearchBox.Text);
+                GetActiveContacts(SearchBox.Text);
             }
             else
             {
-                UpdateDeletedContacts(SearchBox.Text);
+                GetDeletedContacts(SearchBox.Text);
             }
         }
         private void ContactsDataGrid_Loaded(object sender, RoutedEventArgs e)
         {
-            UpdateActiveContacts(SearchBox.Text);
+            GetActiveContacts(SearchBox.Text);
         }
 
         private void WindowActivated(object sender, EventArgs e)
         {
-            UpdateActiveContacts(SearchBox.Text);
+            GetActiveContacts(SearchBox.Text);
         }
 
-        public void UpdateActiveContacts(string where)
+        public void GetActiveContacts(string where)
         {
-            //people = new ObservableCollection<ContactReadDTO>(mapper.Map<IEnumerable<ContactReadDTO>>(repo.GetAllContacts(where)));
+            LocationLabel.Content = "Kontakty";
             people = new ObservableCollection<Contact>(repo.GetAllContacts(where));
             this.ContactsDataGrid.ItemsSource = people;
         }
-        
-        public void UpdateDeletedContacts(string where)
-        {
-            //people = new ObservableCollection<ContactReadDTO>(mapper.Map<IEnumerable<ContactReadDTO>>(repo.GetAllDeletedContacts(where)));
-            people = new ObservableCollection<Contact>(repo.GetAllDeletedContacts(where));
 
-            this.ContactsDataGrid.ItemsSource = people;
+        public void GetDeletedContacts(string where)
+        {
+            LocationLabel.Content = "Kosz";
+            people = new ObservableCollection<Contact>(repo.GetAllDeletedContacts(where));
+            ContactsDataGrid.ItemsSource = people;
         }
 
         private void AddContact_Clik(object sender, RoutedEventArgs e)
         {
-            //repo.CreateContact(new Contact() { FirstName = "Alastor", LastName = "Moody", Email = "defencedarkarts@hogwart.co.uk", Comment = "Do not contact unless emergency" });
-            //repo.SaveChanges();
-
-            //UpdateContacts(SearchBox.Text);
-            AddContactWindow addContactWindow = new AddContactWindow() { Owner = this};
+            AddContactWindow addContactWindow = new AddContactWindow() { Owner = this };
             addContactWindow.ShowDialog();
         }
 
@@ -85,47 +73,50 @@ namespace KsiążkaAdresowaGUI
         {
             showActiveContacts = true;
             SearchBox_SetText("");
-            UpdateActiveContacts(SearchBox.Text);
+            GetActiveContacts(SearchBox.Text);
         }
 
         private void Bin_Clik(object sender, RoutedEventArgs e)
         {
             showActiveContacts = false;
             SearchBox_SetText("");
-            UpdateDeletedContacts(SearchBox.Text);
+            GetDeletedContacts(SearchBox.Text);
+        }
+
+        private object GetDataGridElement(object sender)
+        {
+            var menuItem = (MenuItem)sender;
+            var contextMenu = (ContextMenu)menuItem.Parent;
+            var item = (DataGrid)contextMenu.PlacementTarget;
+
+            return (Contact)item.SelectedCells[0].Item;
+        }
+
+        private void Delete_DGContext(object sender, RoutedEventArgs e)
+        {
+
+            var toDeleteFromDG = (Contact)GetDataGridElement(sender);
+            people.Remove(toDeleteFromDG);
+            toDeleteFromDG.IsDeleted = !toDeleteFromDG.IsDeleted;
+            repo.SaveChanges();
+        }
+
+        private void Edit_DGContext(object sender, RoutedEventArgs e)
+        {
+            selectedEdit = (Contact)GetDataGridElement(sender);
+
+            EditContactWindow addContactWindow = new EditContactWindow() { Owner = this };
+            addContactWindow.ShowDialog();
         }
 
         private void Export_Clik(object sender, RoutedEventArgs e)
         {
-
+            throw new NotImplementedException();
         }
         private void Import_Clik(object sender, RoutedEventArgs e)
         {
-
+            throw new NotImplementedException();
         }
-      
-
-        private void Delete_DGContext(object sender, RoutedEventArgs e)
-        {
-            //Get the clicked MenuItem
-            var menuItem = (MenuItem)sender;
-
-            //Get the ContextMenu to which the menuItem belongs
-            var contextMenu = (ContextMenu)menuItem.Parent;
-
-            //Find the placementTarget
-            var item = (DataGrid)contextMenu.PlacementTarget;
-
-            //Get the underlying item, that you cast to your object that is bound
-            //to the DataGrid (and has subject and state as property)
-            var toDeleteFromBindedList = (Contact)item.SelectedCells[0].Item;
-
-            //Remove the toDeleteFromBindedList object from your ObservableCollection
-            people.Remove(toDeleteFromBindedList);
-            toDeleteFromBindedList.IsDeleted = true;
-            repo.SaveChanges();
-        }
-
 
     }
 }
