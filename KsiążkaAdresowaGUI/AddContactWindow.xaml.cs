4ba1047b713221c7247ed1a417f19249c6b1e431
świdur <dawid@swidurski.pl>
2020-10-12 13:52:29 +0200

    Changed default database path to be current folder

---
+++ KsiążkaAdresowaGUI/AddContactWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using KADataAccess;
+using KADataAccess.Models;
+using KARepository.Infrastructure.Repositories.Implementations;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+
+namespace KsiążkaAdresowaGUI
+{
+    /// <summary>
+    /// Logika interakcji dla klasy AddContactWindow.xaml
+    /// </summary>
+    public partial class AddContactWindow : Window
+    {
+        ContactEFRepo repo = new ContactEFRepo(new KAContext());
+        public AddContactWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (FirstNameTextBox.Text == "")
+            {
+                ResponseLabel.Content = "Podaj imię";
+                return;
+            }
+
+            repo.CreateContact(new Contact()
+            {
+                FirstName = FirstNameTextBox.Text,
+                LastName = LastNameTextBox.Text,
+                Phone = PhoneTextBox.Text,
+                Email = EmailTextBox.Text,
+                Comment = CommentTextBox.Text
+            });
+
+            if (repo.SaveChanges())
+            {
+                FirstNameTextBox.Text =
+                LastNameTextBox.Text =
+                PhoneTextBox.Text =
+                EmailTextBox.Text =
+                CommentTextBox.Text = null;
+                ResponseLabel.Content = "Dodano!";
+
+            }
+        }
+            
+        private void ClearResponseLabel(object sender, TextChangedEventArgs e)
+        {
+            ResponseLabel.Content = null;
+        }
+    }
+}
--- KsiążkaAdresowaGUI/MainWindow.xaml.cs
+++ KsiążkaAdresowaGUI/MainWindow.xaml.cs
@@ -1,20 +1,15 @@
-﻿using KADataAccess;
+﻿using AutoMapper;
+using KADataAccess;
 using KADataAccess.Models;
-using KARepository.Ifrastructure.Repositories.Implementations;
+using KARepository.Infrastructure.Repositories.Implementations;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
+using KARepository.Infrastructure.Profiles;
+using KARepository.DTOs;
+using System.Collections.ObjectModel;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace KsiążkaAdresowaGUI
 {
@@ -23,28 +18,81 @@ namespace KsiążkaAdresowaGUI
     /// </summary>
     public partial class MainWindow : Window
     {
-        List<Contact> people = new List<Contact>();
+        public ObservableCollection<Contact> people;
+
+        ContactEFRepo repo = new ContactEFRepo(new KAContext());
+        Mapper mapper;
+        public bool showActiveContacts = true;
+
         public MainWindow()
         {
-            InitializeComponent();
+            var config = new MapperConfiguration(cfg => cfg.AddProfile(new ContactProfile()));
+            mapper = new Mapper(config);
 
-            var mapper = new IMapper();
+        }
 
-            var repo = new UserEFRepo(new KAContext());
-            people = repo.GetAllContacts().ToList();
+        private void SearchBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (showActiveContacts)
+            {
+                UpdateActiveContacts(SearchBox.Text);
+            }
+            else
+            {
+                UpdateDeletedContacts(SearchBox.Text);
+            }
+        }
+        private void ContactsDataGrid_Loaded(object sender, RoutedEventArgs e)
+        {
+            UpdateActiveContacts(SearchBox.Text);
+        }
 
+        private void WindowActivated(object sender, EventArgs e)
+        {
+            UpdateActiveContacts(SearchBox.Text);
+        }
 
-            ListOfContacts.ItemsSource = people;
+        public void UpdateActiveContacts(string where)
+        {
+            //people = new ObservableCollection<ContactReadDTO>(mapper.Map<IEnumerable<ContactReadDTO>>(repo.GetAllContacts(where)));
+            people = new ObservableCollection<Contact>(repo.GetAllContacts(where));
+            this.ContactsDataGrid.ItemsSource = people;
+        }
+        
+        public void UpdateDeletedContacts(string where)
+        {
+            //people = new ObservableCollection<ContactReadDTO>(mapper.Map<IEnumerable<ContactReadDTO>>(repo.GetAllDeletedContacts(where)));
+            people = new ObservableCollection<Contact>(repo.GetAllDeletedContacts(where));
+
+            this.ContactsDataGrid.ItemsSource = people;
         }
 
         private void AddContact_Clik(object sender, RoutedEventArgs e)
         {
-            throw new NotImplementedException();
+            //repo.CreateContact(new Contact() { FirstName = "Alastor", LastName = "Moody", Email = "defencedarkarts@hogwart.co.uk", Comment = "Do not contact unless emergency" });
+            //repo.SaveChanges();
+
+            //UpdateContacts(SearchBox.Text);
+            AddContactWindow addContactWindow = new AddContactWindow() { Owner = this};
+            addContactWindow.ShowDialog();
         }
 
+        private void SearchBox_SetText(string newText)
+        {
+            SearchBox.Text = newText;
+        }
         private void Contacts_Clik(object sender, RoutedEventArgs e)
         {
+            showActiveContacts = true;
+            SearchBox_SetText("");
+            UpdateActiveContacts(SearchBox.Text);
+        }
 
+        private void Bin_Clik(object sender, RoutedEventArgs e)
+        {
+            showActiveContacts = false;
+            SearchBox_SetText("");
+            UpdateDeletedContacts(SearchBox.Text);
         }
 
         private void Export_Clik(object sender, RoutedEventArgs e)
@@ -55,9 +103,29 @@ namespace KsiążkaAdresowaGUI
         {
 
         }
-        private void Bin_Clik(object sender, RoutedEventArgs e)
-        {
+      
 
+        private void Delete_DGContext(object sender, RoutedEventArgs e)
+        {
+            //Get the clicked MenuItem
+            var menuItem = (MenuItem)sender;
+
+            //Get the ContextMenu to which the menuItem belongs
+            var contextMenu = (ContextMenu)menuItem.Parent;
+
+            //Find the placementTarget
+            var item = (DataGrid)contextMenu.PlacementTarget;
+
+            //Get the underlying item, that you cast to your object that is bound
+            //to the DataGrid (and has subject and state as property)
+            var toDeleteFromBindedList = (Contact)item.SelectedCells[0].Item;
+
+            //Remove the toDeleteFromBindedList object from your ObservableCollection
+            people.Remove(toDeleteFromBindedList);
+            toDeleteFromBindedList.IsDeleted = true;
+            repo.SaveChanges();
         }
+
+
     }
 }
